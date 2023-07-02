df13ac9e2a63a37823f0e6c85b74ac7d78f868a4
RoryWatts <rory.watts@gmail.com>
2023-07-02 16:00:15 +0800

    Added some features / visuals to the frontend.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 let data;
+let timeoutId = null;
+let results = [];
+let sortColumn = null;
+let sortDescending = false;
+const MIN_SEARCH_LENGTH = 3;
+const DEBOUNCE_DELAY = 300; // in milliseconds
 
 // Fetch the data from the JSON file
 fetch('msh_price_guide.json')
@@ -7,16 +13,29 @@ fetch('msh_price_guide.json')
         data = json;
         // Initialize Fuse.js after the data is loaded
         const options = {
-            keys: ['Description', 'ATC Code'], // replace with your column names
+            keys: ['Description'], // replace with your column names
             threshold: 0.2 // adjust for your desired "fuzziness"
         };
         const fuse = new Fuse(data, options);
 
-        // Add an event listener to the search button
-        document.getElementById('search-button').addEventListener('click', (e) => {
-            const searchString = document.getElementById('search').value;
-            const results = fuse.search(searchString);
-            displayResults(results);
+        // Add an event listener to the search input
+        document.getElementById('search').addEventListener('input', (e) => {
+            // Clear any existing timeout
+            clearTimeout(timeoutId);
+
+            // Start a new timeout
+            timeoutId = setTimeout(() => {
+                const searchString = e.target.value;
+
+                // Only search if the search string is long enough
+                if (searchString.length >= MIN_SEARCH_LENGTH) {
+                    results = fuse.search(searchString);
+                    displayResults(results);
+                } else {
+                    results = [];
+                    displayResults([]); // clear the results
+                }
+            }, DEBOUNCE_DELAY);
         });
     });
 
@@ -33,6 +52,16 @@ function displayResults(results) {
         headers.forEach(header => {
             const th = document.createElement('th');
             th.textContent = header;
+            th.addEventListener('click', () => {
+                if (sortColumn === header) {
+                    sortDescending = !sortDescending; // reverse the sort direction
+                } else {
+                    sortColumn = header;
+                    sortDescending = false;
+                }
+                sortResults(sortColumn, sortDescending);
+                displayResults(results);
+            });
             headerRow.appendChild(th);
         });
         thead.appendChild(headerRow);
@@ -49,4 +78,16 @@ function displayResults(results) {
         }
         resultsContainer.appendChild(tr);
     }
+}
+
+// Function to sort results
+function sortResults(column, descending) {
+    results.sort((a, b) => {
+        const aValue = a.item[column];
+        const bValue = b.item[column];
+
+        if (aValue < bValue) return descending ? 1 : -1;
+        if (aValue > bValue) return descending ? -1 : 1;
+        return 0;
+    });
 }
