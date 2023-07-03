f4f6ed9404e5a3b32f39846917b1784ca4b57f0b
RoryWatts <rory.watts@gmail.com>
2023-07-03 17:31:52 +0800

    Changed to datatables for better fuzzy finding

--- app.js
+++ app.js
@@ -7,78 +7,58 @@ const MIN_SEARCH_LENGTH = 3;
 const DEBOUNCE_DELAY = 300; // in milliseconds
 
 // Fetch the data from the JSON file
+// Fetch the data from the JSON file
 fetch('msh_price_guide.json')
     .then(response => response.json())
     .then(json => {
-        data = json;
-        // Initialize Fuse.js after the data is loaded
-        const options = {
-            keys: ['Description'], // replace with your column names
-            threshold: 0.2 // adjust for your desired "fuzziness"
-        };
-        const fuse = new Fuse(data, options);
-
-        // Add an event listener to the search input
-        document.getElementById('search').addEventListener('input', (e) => {
-            // Clear any existing timeout
-            clearTimeout(timeoutId);
-
-            // Start a new timeout
-            timeoutId = setTimeout(() => {
-                const searchString = e.target.value;
-
-                // Only search if the search string is long enough
-                if (searchString.length >= MIN_SEARCH_LENGTH) {
-                    results = fuse.search(searchString);
-                    displayResults(results);
-                } else {
-                    results = [];
-                    displayResults([]); // clear the results
-                }
-            }, DEBOUNCE_DELAY);
+        data = json.map(item => Object.values(item));  // transform your data into an array of arrays
+        $('#results').DataTable({
+            data: data,
+            response: true,
+            scrollX: true, // for horizontal scrolling
+            scrollY: '50vh', // for vertical scrolling. '50vh' means 50% of the viewport height.
+            scrollCollapse: true,
+            // columnDefs property can be used if you need to customize specific columns
+            // for example, it can be used to disable searching or sorting for certain columns
+            columnDefs: []
         });
     });
 
+
 // Function to display results
 function displayResults(results) {
-    const resultsContainer = document.getElementById('results');
-    resultsContainer.innerHTML = ''; // Clear previous results
+    const resultsContainer = $('#results');
+    resultsContainer.empty(); // Clear previous results
 
     // Create table headers based on the first result item
     if (results.length > 0) {
         const headers = Object.keys(results[0].item);
-        const thead = document.createElement('thead');
-        const headerRow = document.createElement('tr');
+        const thead = $('<thead></thead>');
+        const headerRow = $('<tr></tr>');
         headers.forEach(header => {
-            const th = document.createElement('th');
-            th.textContent = header;
-            th.addEventListener('click', () => {
-                if (sortColumn === header) {
-                    sortDescending = !sortDescending; // reverse the sort direction
-                } else {
-                    sortColumn = header;
-                    sortDescending = false;
-                }
-                sortResults(sortColumn, sortDescending);
-                displayResults(results);
-            });
-            headerRow.appendChild(th);
+            const th = $('<th></th>');
+            th.text(header);
+            headerRow.append(th);
         });
-        thead.appendChild(headerRow);
-        resultsContainer.appendChild(thead);
+        thead.append(headerRow);
+        resultsContainer.append(thead);
     }
 
     // Loop through each result and add it to the results container
     for (let item of results) {
-        const tr = document.createElement('tr');
+        const tr = $('<tr></tr>');
         for (let key in item.item) {
-            const td = document.createElement('td');
-            td.textContent = item.item[key];
-            tr.appendChild(td);
+            const td = $('<td></td>');
+            td.text(item.item[key]);
+            tr.append(td);
         }
-        resultsContainer.appendChild(tr);
+        resultsContainer.append(tr);
     }
+
+    // Convert the table to a DataTable
+    resultsContainer.DataTable();
 }
+
 
 // Function to sort results
 function sortResults(column, descending) {
@@ -91,3 +71,4 @@ function sortResults(column, descending) {
         return 0;
     });
 }
+
